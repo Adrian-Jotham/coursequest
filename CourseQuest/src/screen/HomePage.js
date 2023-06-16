17d3ff6ee61d6b3f3587a8c9f6ff441e1a7af554
kazarach <kazarachmain@gmail.com>
2023-06-17 00:27:25 +0700

    Searchbox

--- CourseQuest/src/screen/HomePage.js
+++ CourseQuest/src/screen/HomePage.js
@@ -1,30 +1,76 @@
 import React from 'react';
-import { View, Image, StyleSheet } from 'react-native';
-import LinearGradient from 'react-native-linear-gradient';
+import { View, Image, StyleSheet, Text } from 'react-native';
+import SearchBox from './SearchBox';
 
 const HomePage = () => {
   return (
     <View style={styles.container}>
-      <Image source={require('./Gambar/HomeLogo.png')} style={styles.logo} />
-      <Image source={require('./Gambar/Lonceng.png')} style={styles.logo} />
+      <View style={styles.logoContainer}>
+        <Image
+          source={require('./Gambar/HomeLogo.png')}
+          style={styles.logo}
+        />
+        <Image
+          source={require('./Gambar/Lonceng.png')}
+          style={styles.logoLonceng}
+        />
+      </View>
+      <Text style={styles.textWelcome}>Welcome Traveler,</Text>
+      <Text style={styles.textFind}>Find the best quest for you!</Text>
+      <View style={styles.Search}>
+        <SearchBox/>
+        <View style={styles.logoFilter}>
+          <Image source={require('./Gambar/Filter.png')}/>
+        </View>
+      </View>
+      
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  gradient: {
-    flex: 1,
-  },
-  
   container: {
     flex: 1,
-    backgroundColor: '#DEDEDE',
-    alignItems: 'center',
-    // justifyContent: 'center',
+    backgroundColor: '#D6D6D6',
+    // alignItems: 'center',
+  },
+  logoContainer: {
+    flexDirection: 'row',
+    marginTop: 42,
+    marginLeft: 30,
+    borderBottomColor: 'black',
+    borderBottomWidth: 1,
+    alignItems:'center',
+    width:'85%',
+  },
+  Search:{
+    marginTop:20,
+    marginLeft:15,
   },
   logo: {
-    marginTop:42,
-    marginRight:120,
+    marginRight: 54,
+  },
+  logoLonceng: {
+    marginBottom: 10,
+  },
+  logoFilter: {
+    // marginBottom: 10,
+    alignItems:'flex-end'
+
+  },
+  textWelcome: {
+    textAlign: 'left',
+    marginTop:27,
+    marginLeft:30,
+    fontSize:15,
+    color:'#5C2605',
+  },
+  textFind:{
+    textAlign: 'left',
+    marginTop:10,
+    marginLeft:30,
+    fontSize:20,
+    color:'#5C2605',
   },
 });
 
---
+++ CourseQuest/src/screen/SearchBox.js
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+import { TextInput, View, Image, StyleSheet, TouchableOpacity } from 'react-native';
+
+const SearchBox = () => {
+  const [searchText, setSearchText] = useState('');
+
+  const handleSearch = () => {
+    // Lakukan operasi pencarian atau perbarui hasil pencarian menggunakan nilai searchText
+    console.log('Lakukan pencarian:', searchText);
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.innerContainer}>
+      <TouchableOpacity style={styles.searchButton} onPress={handleSearch}>
+          {/* <Icon name="search" size={20} color="#fff" /> */}
+          <Image source={require('./Gambar/Search.png')} />
+        </TouchableOpacity>
+        <TextInput
+          style={styles.input}
+          placeholder="Search a New Quest"
+          value={searchText}
+          onChangeText={setSearchText}
+        />
+        
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    // justifyContent: 'center',
+    // alignItems: 'center',
+    paddingHorizontal: 16,
+    marginRight:70,
+    height:36,
+    // flex:1,
+  },
+  innerContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderWidth: 1,
+    borderColor: 'black',
+    borderRadius: 20,
+    paddingHorizontal: 10,
+    backgroundColor:'white',
+  },
+  input: {
+    flex: 1,
+    height: 40,
+    borderRadius: 20,
+    marginRight: 10,
+  },
+  searchButton: {
+    padding: 5,
+    marginLeft: 5,
+    borderRadius: 20,
+  },
+});
+
+export default SearchBox;
